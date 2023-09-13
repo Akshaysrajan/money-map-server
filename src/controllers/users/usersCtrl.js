4939c7c5338acc9cc2afadb951043503c2315eec
akshay s rajan <akshaysrajanofficial@gmail.com>
2023-09-13 12:08:43 +0530

    update

--- src/controllers/users/usersCtrl.js
+++ src/controllers/users/usersCtrl.js
@@ -1,4 +1,5 @@
 const expressAsyncHandler = require('express-async-handler');
+const generateToken = require("../../middlewares/generateToken");
 const User = require("../../model/User");
 
 
@@ -31,4 +32,26 @@ const fetchUsersCtrl =expressAsyncHandler(  async (req, res) => {
     }
 });
 
-module.exports ={registerUser, fetchUsersCtrl};
+//login user
+const loginUserCtrl = expressAsyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    //check if user exists
+    const userFound = await User.findOne({ email });
+    //Check if password is match
+    if (userFound && (await userFound?.isPasswordMatch(password))) {
+      res.json({
+        _id: userFound?._id,
+        firstName: userFound?.firstName,
+        lastName: userFound?.lastName,
+        email: userFound?.email,
+        isAdmin: userFound?.isAdmin,
+        token: generateToken(userFound?._id),
+      });
+    } else {
+      res.status(401);
+      throw new Error("Invalid Login Credentials");
+    }
+  });
+  
+
+module.exports ={registerUser, fetchUsersCtrl, loginUserCtrl};
